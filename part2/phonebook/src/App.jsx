53e0851daca93d2d00544cc34bc405dc70195fda
AntoineDujar <antoine.v.dujardin@gmail.com>
2024-01-09 00:52:42 -0500

    Exercise 2.16

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,11 +4,24 @@ import People from './People'
 import PeopleForm from './PeopleForm'
 import Filter from './Filter'
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newSearch, setNewSearch] = useState("");
+  const [notification, setNotification] = useState(null);
 
   useEffect(() => {
     // console.log('effect')
@@ -55,6 +68,13 @@ const App = () => {
           setPersons(persons.concat(response));
           setNewName("");
           setNewNumber("");
+
+          setNotification(
+            `Note '${personObject.name}' was added to server`
+          )
+          setTimeout(() => {
+            setNotification(null)
+          }, 5000)
         })
         .catch(error => {
           console.log('problem with post')
@@ -87,6 +107,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notification} />
       <Filter newSearch={newSearch} handleSearchChange={handleSearchChange}/>
       <h2>add a new</h2>
       <PeopleForm 
